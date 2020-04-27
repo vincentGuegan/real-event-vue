449ed51e6cf5aff066929b415c154011a52357fe
Vincent Guegan <gueganvincent@gmail.com>
2020-04-27 17:00:37 +0200

    3.5) Fetch list of Events with Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -26,12 +26,24 @@ export default new Vuex.Store({
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
     }
   },
   actions: {
     createEvent({ commit }, event) {
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
+      })
+    },
+    fetchEvents({ commit }) {
+      EventService.getEvents()
+      .then(response => {
+      commit('SET_EVENTS', response.data)
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response) // Logs out the error
       })
     }
   },

47c95f02241fa420bdb16e65a2f18cf728a01bd7
Vincent Guegan <gueganvincent@gmail.com>
2020-04-27 12:43:13 +0200

    3.3) Vuex: State & Getters revised

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,30 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Vincent GUEGAN' },
+    categories: ['sustainability', 
+                'nature', 
+                'animal welfare', 
+                'housing', 
+                'education', 
+                'food', 
+                'community'
+              ],
+
+    events: [
+      { id: 1, title: '...', organizer: true },
+      { id: 2, title: '...', organizer: false },
+      { id: 3, title: '...', organizer: true },
+      { id: 4, title: '...', organizer: false }
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 });

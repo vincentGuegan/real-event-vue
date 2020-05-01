3830ca0ba65abfa34ae1b87cfe7d728d965af2bc
Vincent Guegan <gueganvincent@gmail.com>
2020-05-01 10:37:03 +0200

    4.2) Progress Bar: installing NProgress, using Axios interceptors

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const apiClient = axios.create({ // a single Axios instance for our entire app
     baseURL: 'http://localhost:3000', // Base URLfor all calls to use
@@ -7,6 +8,16 @@ const apiClient = axios.create({ // a single Axios instance for our entire app
         Accept: 'application/json',         //for authentication...
         'Content-Type': 'application/json' //...& configuration
     }
+})
+
+apiClient.interceptors.request.use(config => {
+    NProgress.start()
+    return config
+})
+
+apiClient.interceptors.response.use(response => {
+    NProgress.done()
+    return response
 })
 
 export default {

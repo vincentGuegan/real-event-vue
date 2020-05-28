e348443055fe2116fea9cb40980d02d234975371
Vincent Guegan <gueganvincent@gmail.com>
2020-05-28 12:39:40 +0200

    4.8) Reusable Form Components: BaseSelect

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label v-if="label">{{ label }}</label>
+        <select :value="value" @input="updateValue" v-bind="$attrs"> <!-- When the input event happens, it will trigger the updateValue method which sounds up the payload -->
+            <option
+                v-for="option in options"
+                :value="option"
+                :key="option"
+                :selected="option === value" 
+                >{{ option }}</option>
+        </select>
+    </div>
+</template>
+ 
+<script>
+export default {
+    inheritAttrs: false,
+    props: {
+        options: { // options which can be selected
+            type: Array,
+            required: true,
+        },
+        label: {
+            type: String, 
+            default: ""
+        },
+        value: [String, Number]
+    },
+    methods: {
+        updateValue(event) {
+            this.$emit("input", event.target.value)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

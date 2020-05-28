6bc1b1f24b5e05a3393b9d03e14a9201fca7d3c3
Vincent Guegan <gueganvincent@gmail.com>
2020-05-28 12:09:25 +0200

    4.7) Reusable Form Components: BaseInput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <label v-if="label">{{ label }}</label>
+        <input :value="value" @input="updateValue" v-bind="$attrs"> <!-- When the input event happens, it will trigger the updateValue method which sounds up the payload -->
+    </div>
+</template>
+ 
+<script>
+export default {
+    inheritAttrs: false,
+    props: {
+        label: {
+            type: String, 
+            default: ""
+        },
+        value: [String, Number]
+    },
+    methods: {
+        updateValue(event) {
+            this.$emit("input", event.target.value)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
